f7acd90b0265a7b1b659898dfc063e216e371329
Chris Childress <chris.childress@stratusgrid.com>
2021-07-27 13:18:47 -0400

    update to ignore node_modules folders

---
+++ secret-bitbucket-secrets.tf
@@ -0,0 +1,30 @@
+# This secret will store sensitive values which must be retrieved by the downloader lambda.
+# Specifically, the bitbucket access token and signing secret.
+
+resource "aws_secretsmanager_secret" "bitbucket_pat_and_signing_key" {
+  name = "${var.name}-bitbucket-secret"
+  description = "Personal Access Token and Webhook Signing Key for Bitbucket."
+
+  tags = merge(var.input_tags,
+    {
+      "Name" = "${var.name}-bitbucket-secret"
+    }
+  )
+}
+
+locals {
+  bitbucket_tokens = jsonencode({
+    "bitbucket_secret" : var.lambda_bitbucket_secret,
+    "bitbucket_token" : var.lambda_bitbucket_access_token
+  })
+}
+
+resource "aws_secretsmanager_secret_version" "bitbucket_pat_and_signing_key" {
+  secret_id = aws_secretsmanager_secret.bitbucket_pat_and_signing_key.id
+  secret_string = local.bitbucket_tokens
+}
+
+data "aws_secretsmanager_secret_version" "bitbucket_pat_and_signing_key" {
+  depends_on = [aws_secretsmanager_secret_version.bitbucket_pat_and_signing_key]
+  secret_id = aws_secretsmanager_secret.bitbucket_pat_and_signing_key.id
+}
